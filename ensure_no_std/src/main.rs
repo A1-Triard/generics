8c3038b258271f200522330b46bd0acbfe71bf78
Warlock <internalmike@gmail.com>
2022-05-04 12:41:29 +0300

    update

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -7,8 +7,6 @@
 
 use core::alloc::Layout;
 use core::panic::PanicInfo;
-use educe::Educe;
-use phantom_type::*;
 #[cfg(not(windows))]
 use libc::exit;
 use libc_alloc::LibcAlloc;
@@ -40,15 +38,44 @@ pub fn rust_oom(_layout: Layout) -> ! {
     unsafe { exit(98) }
 }
 
-struct NonClonable;
+trait TheTrait { }
 
-#[derive(Educe)]
-#[educe(Clone, Copy)]
-struct Test<T>(PhantomType<T>);
+use generics::parse as generics_parse;
+#[allow(unused_imports)]
+use core::compile_error as std_compile_error;
+
+#[macro_export]
+macro_rules! impl_the_trait {
+    (
+        $name:ident $($token:tt)*
+    ) => {
+        $crate::generics_parse! {
+            $crate::impl_the_trait {
+                @impl $name
+            }
+            $($token)*
+        }
+    };
+    (
+        @impl $name:ident [$($g:tt)*] [$($r:tt)*] [$($w:tt)*]
+    ) => {
+        impl $($g)* $crate::TheTrait for $name $($r)* $($w)* { }
+    };
+    (
+        @impl $name:ident [$($g:tt)*] [$($r:tt)*] [$($w:tt)*] $($token:tt)+ 
+    ) => {
+        $crate::std_compile_error!(
+            "invalid input, allowed input is '$name $( < $generics > $(where $where_clause)? )?'"
+        );
+    };
+}
+
+struct Test<T: Clone>(T);
+
+impl_the_trait!(Test<T: Clone>);
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let test: Test<NonClonable> = Test(PhantomType::new());
-    let _test_clone = test.clone();
+    let _ = Test(0i32);
     0
 }
